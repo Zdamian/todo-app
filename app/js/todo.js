05ed8106c864c2cfdf8d2a50992f243a1864a56e
DamianZielinski <Damian.Zielinski@interia.pl>
2016-11-23 18:13:19 +0100

    added javascript for app

---
+++ app/js/todo.js
@@ -0,0 +1,107 @@
+$(function() {
+
+	var DOM = {
+		$input: $('.app-input'),
+		$list: $('.app-list'),
+		$button: $('.app-add'),
+	};
+
+	var ENTER = 13;
+	var editMode = false;
+	var itemId;
+
+	DOM.$input.on('keyup', function(e) {
+
+		if (e.keyCode === ENTER) {
+
+			var todo = DOM.$input.val();
+
+			if (editMode) {
+
+				if (todo.trim()) {
+					DOM.$list.find('li:eq(' + itemId + ')').find('.app-todo').text(todo);
+				}
+
+			} else {
+
+				if (todo.trim()) {
+
+					var $item = $('<li/>');
+					var $todo = $('<span class="app-todo"></span>');
+					var $edit = $('<span class="app-edit">E</span>');
+					var $del = $('<span class="app-delete">X</span>');
+
+					$todo.text(todo);
+					$item.append($todo);
+					$item.append($edit);
+					$item.append($del);
+
+					DOM.$list.append($item);
+				}
+			}
+
+			editMode = false;
+			DOM.$input.val('');
+			DOM.$input.focus();
+		}
+
+	});
+
+	DOM.$button.on('click', function(e) {
+
+		var todo = DOM.$input.val();
+
+		if (editMode) {
+			if (todo.trim()) {
+				DOM.$list
+					.find('li:eq(' + itemId + ')')
+					.find('.app-todo')
+					.text(todo);
+			}
+
+		} else {
+
+			if (todo.trim()) {
+
+				var $item = $('<li/>');
+
+				var $todo = $('<span class="app-todo"></span>');
+				var $edit = $('<span class="app-edit">E</span>');
+				var $del = $('<span class="app-delete">X</span>');
+
+				$todo.text(todo);
+
+				$item.append($todo);
+				$item.append($edit);
+				$item.append($del);
+
+				DOM.$list.append($item);
+
+			}
+		}
+
+		editMode = false;
+		DOM.$input.val('');
+		DOM.$input.focus();
+
+	});
+
+	DOM.$list.on('click', '.app-delete', function(e) {
+
+		$(this).parent('li').remove();
+
+	});
+
+	DOM.$list.on('click', '.app-edit', function(e) {
+
+		var todo = $(this).parent('li').find('.app-todo').text();
+
+		DOM.$input.val(todo);
+		DOM.$input.focus();
+
+		editMode = true;
+		itemId = $(this).parent('li').prevAll().length;
+
+	});
+
+});
